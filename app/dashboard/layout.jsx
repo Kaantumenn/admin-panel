0fdeb3895354282fbe9d2e12f92adf555b3e0b89
Kaaantumen <kaan.tumen@kroppa.com>
2023-11-29 14:47:27 +0300

    Sidebar and Navbar completed.

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,16 +1,17 @@
-import Navbar from "../ui/dashboard/navbar/navbar"
-import SideBar from "../ui/dashboard/sidebar/sidebar"
+import Navbar from '../ui/dashboard/navbar/navbar'
+import SideBar from '../ui/dashboard/sidebar/sidebar'
+import styles from '../ui/dashboard/dashboard.module.css'
 
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <div>
-        <div>
-            <SideBar/>
-        </div>
-        <div>
-            <Navbar />
-            {children}
-        </div>
+    <div className={styles.container}>
+      <div className={styles.menu}>
+        <SideBar />
+      </div>
+      <div className={styles.content}>
+        <Navbar />
+        {children}
+      </div>
     </div>
   )
 }
